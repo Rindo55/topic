beefeb9bca8d7c5a5513b74f937ca8d84eca8519
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-05-01 16:23:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
-import os
+from os import getenv
 
+from dotenv import load_dotenv
 
+load_dotenv()
 
-class Config(object):
+API_ID = "10247139"
 
-      API_ID = "10247139"
+API_HASH = "96b46175824223a33737657ab943fd6a"
 
-      API_HASH = "96b46175824223a33737657ab943fd6a"
-      BOT_TOKEN = "5706217182:AAELnRWjsHatV-yJ5MvNgb7yAz2-QF6EWf8"
+BOT_TOKEN = "5706217182:AAELnRWjsHatV-yJ5MvNgb7yAz2-QF6EWf8"
