43cd370cc89feb71c754d7910c74e6b906bd213f
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-05-01 16:35:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,6 +16,29 @@ app = Client(
 app.start()
 
 loop = asyncio.get_event_loop()
+def b64_to_str(b64: str) -> str:
+    bytes_b64 = b64.encode('ascii')
+    bytes_str = standard_b64decode(bytes_b64)
+    __str = bytes_str.decode('ascii')
+    return __str
+
+@app.on_message(filters.command("start") & filters.private)
+async def start(bot, cmd: Message):
+    usr_cmd = cmd.text.split("_", 1)[-1]
+    kay_id = -1001642923224
+    if usr_cmd == "/start":
+       await cmd.reply_text("Bot seems online! ⚡️")
+    else:
+        try:
+            try:
+                file_id = int(b64_to_str(usr_cmd).split("_")[-1])
+            except (Error, UnicodeDecodeError):
+                file_id = int(usr_cmd.split("_")[-1])
+            GetMessage = await app.get_messages(kay_id, message_ids=file_id)
+            message_ids = GetMessage.message_id
+            await app.copy_message(chat_id=cmd.from_user.id, from_chat_id=kay_id, message_id=message_ids)
+        except Exception as err:
+            await cmd.reply_text(f"Something went wrong!\n\n**Error:** `XXXXXXX`")
 
 async def start_bot():
   print("==================================")
