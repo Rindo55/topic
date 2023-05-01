d88535a790107da3afb8a7283876f340b195f771
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-05-01 16:26:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,3 +9,21 @@ app = Client(
     bot_token=BOT_TOKEN
   )
 app.start()
+
+async def start_bot():
+  print("==================================")
+  print("[INFO]: AutoAnimeBot Started Bot Successfully")
+  print("==========JOIN @Latest_ongoing_airing_animes=========")
+  
+  await idle()
+  print("[INFO]: BOT STOPPED")
+  await app.stop()  
+  for task in asyncio.all_tasks():
+    task.cancel()
+
+if __name__ == "__main__":
+  install()
+  with closing(loop):
+    with suppress(asyncio.exceptions.CancelledError):
+      loop.run_until_complete(start_bot())
+      loop.run_until_complete(asyncio.sleep(3.0))
