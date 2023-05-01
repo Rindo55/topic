15ba16a1ecf0b3c96613f338f6b61d896b6ae5eb
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-05-01 16:23:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,29 +1,11 @@
-import pyrogram
+from pyrogram import Client
+from config import *
+import libtorrent as lt
 
-import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
-import os
-
-from config import Config
-from pyrogram import Client 
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
-class app(Client):
-    
-    def __init__(self):
-        super().__init__(
-            session_name="Captioner",
-            bot_token = Config.BOT_TOKEN,
-            api_id = Config.API_ID,
-            api_hash = Config.API_HASH,
-            workers = 20,
-            plugins = dict(
-                root="Plugins"
-            )
-        )
-
-if __name__ == "__main__" :
-    app().run()
+app = Client(
+    "bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+  )
+app.start()
