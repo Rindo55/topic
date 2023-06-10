0453c2740c200b85ff9e2965c8996ff75b753f75
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-06-10 11:38:27 +0530

    Update runner.py

--- Plugins/runner.py
+++ Plugins/runner.py
@@ -1,13 +1,29 @@
 import asyncio
 import os
+import re
 import time
 import aiohttp
 import requests
 import aiofiles
-from Plugins.utils import b64_to_str
+from base64 import standard_b64encode, standard_b64decode
 from pyrogram import Client, filters
 
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+
+def b64_to_str(b64: str) -> str:
+    bytes_b64 = b64.encode('ascii')
+    bytes_str = standard_b64decode(bytes_b64)
+    __str = bytes_str.decode('ascii')
+    return __str
+def str_to_b64(__str: str) -> str:
+
+    str_bytes = __str.encode('ascii')
+
+    bytes_b64 = standard_b64encode(str_bytes)
+
+    b64 = bytes_b64.decode('ascii')
+
+    return b64
 
 @app.on_message(filters.command("start") & filters.private)
 async def start(bot, cmd: Message):
@@ -26,3 +42,24 @@ async def start(bot, cmd: Message):
             await app.copy_message(chat_id=cmd.from_user.id, from_chat_id=kay_id, message_id=message_ids)
         except Exception as err:
             await cmd.reply_text(f"Something went wrong!\n\n**Error:** `XXXXXXX`")
+            
+            
+@app.on_message(filters.command("link") & filters.private)
+
+async def link(bot, cmd: Message):
+    usr_cmd = cmd.text.split("_", 1)[-1]
+    if usr_cmd == "/link":
+
+       await cmd.reply_text("Fuck off!")
+
+    else: 
+        try:
+
+       
+            fuk_cmd = cmd.text.replace("/link https://t.me/c/1642923224/", "")
+            filex_id = str_to_b64(fuk_cmd)
+            sendx = await app.send_message(chat_id=cmd.from_user.id, text="https://t.me/somayukibot?start=animxt_" + filex_id)
+        except Exception as err:
+            await cmd.reply_text(f"Something went wrong!\n\n**Error:** `XXXXXXX`")
+
+    
