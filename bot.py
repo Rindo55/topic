f4c52fdab5a1e027ec8da8f047224b685ffc8c34
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-06-10 12:00:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,49 +1,80 @@
-import pyrogram
-
-import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
+import asyncio
 import os
+import re
+import time
+import aiohttp
+import requests
+import aiofiles
+from base64 import standard_b64encode, standard_b64decode
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+import logging
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+
 
 from config import Config
-from pyrogram import Client 
+
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
-class app(Client):
-   
-
-    def __init__(self):
-        super().__init__(
-
-            session_name="Captioner",
-
+app = Client(
+            "Bot",
             bot_token = Config.BOT_TOKEN,
-
             api_id = Config.API_ID,
+            api_hash = Config.API_HASH)
 
-            api_hash = Config.API_HASH,
-            workers = 20,
+def b64_to_str(b64: str) -> str:
+    bytes_b64 = b64.encode('ascii')
+    bytes_str = standard_b64decode(bytes_b64)
+    __str = bytes_str.decode('ascii')
+    return __str
+def str_to_b64(__str: str) -> str:
 
-            plugins = dict(
+    str_bytes = __str.encode('ascii')
 
-                root="Plugins"
+    bytes_b64 = standard_b64encode(str_bytes)
 
-            )
+    b64 = bytes_b64.decode('ascii')
 
-        )
+    return b64
 
-           
+@app.on_message(filters.command("start") & filters.private)
+async def start(bot, cmd: Message):
+    usr_cmd = cmd.text.split("_", 1)[-1]
+    kay_id = -1001642923224
+    if usr_cmd == "/start":
+       await cmd.reply_text("Bot seems online! ⚡️")
+    else:
+        try:
+            try:
+                file_id = int(b64_to_str(usr_cmd).split("_")[-1])
+            except (Error, UnicodeDecodeError):
+                file_id = int(usr_cmd.split("_")[-1])
+            GetMessage = await app.get_messages(kay_id, message_ids=file_id)
+            message_ids = GetMessage.message_id
+            await app.copy_message(chat_id=cmd.from_user.id, from_chat_id=kay_id, message_id=message_ids)
+        except Exception as err:
+            await cmd.reply_text(f"Something went wrong!\n\n**Error:** `XXXXXXX`")
+            
+            
+@app.on_message(filters.command("link") & filters.private)
 
-   
+async def link(bot, cmd: Message):
+    usr_cmd = cmd.text.split("_", 1)[-1]
+    if usr_cmd == "/link":
 
+       await cmd.reply_text("Fuck off!")
 
+    else: 
+        try:
 
-
+       
+            fuk_cmd = cmd.text.replace("/link https://t.me/c/1642923224/", "")
+            filex_id = str_to_b64(fuk_cmd)
+            sendx = await app.send_message(chat_id=cmd.from_user.id, text="https://t.me/somayukibot?start=animxt_" + filex_id)
+        except Exception as err:
+            await cmd.reply_text(f"Something went wrong!\n\n**Error:** `XXXXXXX`")
 
     
-    
-
-
-if __name__ == "__main__":
-    app().run()
+app.start()
+print("Powered by @animxt")
+idle()
