4304d27058d170d6464c24a31f5887deeef5ae97
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-06-10 11:03:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -82,8 +82,4 @@ async def start_bot():
     task.cancel()
 
 if __name__ == "__main__":
-  install()
-  with closing(loop):
-    with suppress(asyncio.exceptions.CancelledError):
-      loop.run_until_complete(start_bot())
-      loop.run_until_complete(asyncio.sleep(3.0))
+  app.run()
